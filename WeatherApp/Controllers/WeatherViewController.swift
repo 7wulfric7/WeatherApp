71cf8743e25cc2351091f589d10ad717c3ba69ab
Deniz Adil <xdenizadil@gmail.com>
2020-09-30 19:22:44 +0200

    Navigation bar added, Tab bar view controller added, The view in general added

---
+++ WeatherApp/Controllers/WeatherViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherViewController.swift
+//  WeatherApp
+//
+//  Created by Deniz Adil on 9/30/20.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
